7ca81d7e64d22f40751cb19773fe1f515034874f
Douglas Amon <douglasamon12@gmail.com>
2026-04-22 22:09:50 -0300

    ArrayQueue implementation and methods

--- Filas-FIFO/ArrayQueue.go
+++ Filas-FIFO/ArrayQueue.go
@@ -1,78 +1,69 @@
-package queue
+package main
 
 import (
 	"errors"
+	"fmt"
 )
 
+// circularidade da fila -> index := (q.front + i) % len(q.data)
+
 type ArrayQueue struct {
-	v     []int
+	data  []int
 	front int
 	rear  int
-	// inserted int
+	size  int
 }
 
-func (queue *ArrayQueue) Init(size int) {
-	queue.v = make([]int, size)
-	queue.front = -1
-	queue.rear = -1
+func (q *ArrayQueue) Init(capacity int) {
+	q.data = make([]int, capacity)
+	q.front = 0
+	q.rear = 0
+	q.size = 0
 }
 
-func (queue *ArrayQueue) Size() int {
-	if queue.front == -1 && queue.rear == -1 { // fila vazia
-		return 0
+func (q *ArrayQueue) Enqueue(value int) {
+	if q.size == len(q.data) {
+		fmt.Println("erro: fila cheia")
+		return
 	}
-	if queue.rear >= queue.front { // nao deu a volta
-		return queue.rear - queue.front + 1
-	}
-	return len(queue.v) - queue.front + queue.rear + 1 // deu a volta
+
+	q.data[q.rear] = value
+	q.rear = (q.rear + 1) % len(q.data)
+	q.size++
 }
 
-func (queue *ArrayQueue) doubleV() {
-	n := len(queue.v)
-	newV := make([]int, n*2)
-	size := queue.Size()
-	i := queue.front
-	for j := 0; j < size; j++ {
-		newV[j] = queue.v[i]
-		i = (i + 1) % n
+func (q *ArrayQueue) Dequeue() (int, error) {
+	if q.IsEmpty() {
+		return 0, errors.New("fila vazia")
 	}
-	queue.v = newV
-	queue.front = 0
-	queue.rear = size - 1
+
+	value := q.data[q.front]
+	q.front = (q.front + 1) % len(q.data)
+	q.size--
+
+	return value, nil
 }
 
-func (queue *ArrayQueue) Push(val int) {
-	if queue.front == -1 && queue.rear == -1 { // fila vazia
-		queue.front++
-		queue.rear++
-	} else {
-		queue.rear = (queue.rear + 1) % len(queue.v)
+func (q *ArrayQueue) Front() (int, error) {
+	if q.IsEmpty() {
+		return 0, errors.New("fila vazia")
 	}
-	queue.v[queue.rear] = val
+	return q.data[q.front], nil
 }
 
-func (queue *ArrayQueue) Pop() (int, error) {
-	if queue.front == -1 && queue.rear == -1 {
-		return -1, errors.New("Fila vazia")
-	}
-	val := queue.v[queue.front]
-	if queue.front == queue.rear { // unico elemento na fila
-		queue.front, queue.rear = -1, -1
-		return val, nil
-	}
-
-	// avanço circular
-	queue.front = (queue.front + 1) % len(queue.v)
-	return val, nil
+func (q *ArrayQueue) IsEmpty() bool {
+	return q.size == 0
 }
 
-func (queue *ArrayQueue) Peek() (int, error) {
-	if queue.front == -1 && queue.rear == -1 {
-		return -1, errors.New("Fila vazia")
-	}
-	return queue.v[queue.front], nil
+func (q *ArrayQueue) Size() int {
+	return q.size
 }
 
-func (queue *ArrayQueue) IsEmpty() bool {
-	return queue.front == -1 && queue.rear == -1
+func (q *ArrayQueue) Print() {
+	fmt.Print("Fila: ")
+	for i := 0; i < q.size; i++ {
+		index := (q.front + i) % len(q.data)
+		fmt.Print(q.data[index], " ")
+	}
+	fmt.Println()
 }
